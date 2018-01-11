d6a80798e8d0ddd8bfe4e05ed6eeb3b4e750c9e5
hernandoferry <hernando.ferry@gmail.com>
2018-01-11 17:33:23 +0700

    tugas 2 buat web bootstrap

--- OOP/enkapsulasi_external.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include_once("Ferry.class.php");
-$enkapsulasi_external = new identitas();
-echo "Enkapsulasi di akses dari external class";
-echo $enkapsulasi_external->extra_info();
-
-
-?>
